397532b34bb90296c6f1f254e1d647d59900a640
thulshof <t.hulshof@student.ru.nl>
2020-03-05 15:02:18 +0100

    Initial Commit

---
+++ SwiftUISyntax/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftUISyntax
+//
+//  Created by Toine Hulshof on 05/03/2020.
+//  Copyright © 2020 Toine Hulshof. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
